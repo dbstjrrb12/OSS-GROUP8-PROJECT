23faee7c9219ce48ff0411d55dbe7fadfb43016b
yoon <dbstjrrb12@naver.com>
2019-05-23 18:56:33 +0900

    new

--- main.c
+++ main.c
@@ -27,6 +27,7 @@ typedef struct {
 } tok_t;
 
 tok_t token;
+token.size = 0;
 
 int main(int argc, char* argv[]) {
     
@@ -77,17 +78,17 @@ int main(int argc, char* argv[]) {
         i++;
     }
     
-    Parser(n,buff);
+    Parser(0,n,buff);
     return 0;
 }
 
-void Parser(int size, char *buff)
+void Parser(int start, int end, char *buff)
 {
     int i = 0;
     int j = 0;
     int cnt = 0;
     
-    while(i != size){
+    while(i != end){
         //buff[n] = d;
         // n++;
         
@@ -105,49 +106,82 @@ void Parser(int size, char *buff)
     }
     i++;
     
-    //  printf("%d", size);
-    
-    while(i < size){
-        // printf("while\n");
-        
-        switch ( buff[i] ) {
-            case '"':
-                // printf("1");
-                token.start = i + 1;
-                token.type = 3;
-                //printf("2");
-                j = i + 1;
-                cnt = i + 1;
+    while(i < end){        
+     switch ( buff[i] ) {
+	    case '{': 
+		   token.start = i + 1;
+		   token.type = 1;
+		   
+
+		   while((buff[j] != '}')){
+	  	    if(buff[j] == ',') token.size += 1;
+            if(buff[j] == '{'){
+      			cnt = j;
+			    while(buff[cnt] != '}') cnt++; // nested object size 			    	    
+			    parser(j,cnt,tmp);
+            }
+            else{
+                j++;
+            }
+           }
+           token.end = j; 
+           parser(token.start, j, buff); // distinguish element in the Object
+		   break;
+
+	    case '[': 
+		   token.start = i + 1;
+		   token.type = 1;
+		   
+
+		   while((buff[j] != ']')){
+	  	    if(buff[j] == ',') token.size += 1;
+            if(buff[j] == '['){
+      			cnt = j;
+			    while(buff[cnt] != ']') cnt++; // nested object size 			    	    
+			    parser(j,cnt,tmp);
+            }
+            else{
+                j++;
+            }
+           }
+           token.end = j; 
+           parser(token.start, j, buff); // distinguish element in the Object
+		   break;
+		
+        case '"':    
+            token.start = i + 1;
+            token.type = 3;
+            
+            j = i + 1;
+            cnt = i + 1;
  
-                while((buff[j] != '"')){
-                    if(buff[j] == '\\'){
-                        if(buff[j + 1] == '"'){
-                            j += 2;
-                            cnt++;
-                        }
-                    }
-                    else{
-                        j++;
-                        cnt++;
-                    }
+            while((buff[j] != '"')){
+                if(buff[j] == '\\'){
+                  if(buff[j + 1] == '"'){
+                    j += 2;
+                    cnt++;
+                  }
                 }
+                else{
+                    j++;
+                    cnt++;
+                }
+            }
+		    if(buff[j+1] == ':') token.size = 1;
+		    else if(buff[j+1] == ',') token.size = 0;
                 
-                i = j;
-                // printf("3");
-                token.end = cnt;
-                //                printf("end  = %d\n", i);
-                token.size = token.end - token.start + 1;
-                
-                
-                for(int a = token.start; a < token.end; a++)
-                {
-                    if(buff[a] == '\\'){
-                        a += 1;
-                        switch(buff[a]){
-                            case '"':
-                                printf("%c", buff[a]);
-                                a++;
-                                break;
+            i = j;
+            
+            token.end = cnt;
+            for(int a = token.start; a < token.end; a++)
+            {
+                if(buff[a] == '\\'){
+                    a += 1;
+                    switch(buff[a]){
+                        case '"':
+                          printf("%c", buff[a]);
+                          a++;
+                          break;
                                 //                           case '\':
                                 //                               i++;
                                 //                               printf("%c", buff[a]);
@@ -194,18 +228,39 @@ void Parser(int size, char *buff)
                 printf("start = %d, end =%d \n", token.start, token.end);
                 break;
                 
-            case '{':
-                
-            default: break;
-                
-                // case '{' :
-                
-                //   break;
-        }
-        i++;
-    }
-    
-    
+        default: 
+            if(buff[i] == 'T') {
+                token.start = i;
+                char * tmp = (char*)malloc(sizeof(char)*5);
+                int cnt = i;
+                for(int temp = 0; cnt < cnt+4; temp++, cnt++) 
+                    tmp[temp] = buff[cnt];
+                tmp[4] = '\0'; // null 
+                if(!strcmp(tmp, 'TRUE')){
+                    token.type = 4;
+                    token.end = cnt;
+                    token.size = 0;
+                    i = cnt;
+                }
+            }
+            else if(buff[i] == 'F'){
+                token.start = i;
+                char * tmp = (char*)malloc(sizeof(char)*6);
+                int cnt = i;
+                for(int temp = 0; cnt < cnt+5; temp++, cnt++) 
+                    tmp[temp] = buff[cnt];
+                tmp[5] = '\0'; // null 
+                if(!strcmp(tmp, 'FALSE')){
+                    token.type = 4;
+                    token.end = cnt;
+                    token.size = 0;
+                    i = cnt;
+                }
+            }
+            break;
+     }
+     i++;
+    }   
 }
 
 
