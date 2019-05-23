7f329bf32d71398b0a40babc909434939381f4b5
JuneAlina <21500375@handong.edu>
2019-05-23 23:51:35 +0900

    commit

--- main.c
+++ main.c
@@ -13,7 +13,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Parser(int start,int end, char * buff);
+void Parser(int size, int start, char * buff);
+
+int start = 0;
 
 typedef enum {
     UNDEFINED = 0,
@@ -33,89 +35,89 @@ typedef struct {
 tok_t token;
 
 int main(int argc, char* argv[]) {
-
-
+    
+    
     // 옵션 지정하지 않았을 때 에러 출력하고 종료
     if (argc == 1) {
         fputs("No Input\n", stderr);
         exit(1);
     }
-
-
+    
+    
     // 옵션 개수 출력
     printf("%d 개의 argv\n\n", argc - 1);
-
-
+    
+    
     // 옵션 배열의 요소들을 하나씩 출력
     for (int i = 1; i < argc; i++)
         printf("argv[%d] = %s\n", i, argv[i]);
-
-
+    
+    
     FILE * fp = fopen(argv[1], "r");
     //char buff[1024];
     char d;
     int n = 0;
-
+    
     if (fp == NULL)
     {
         perror("Error while opening the file.\n");
         exit(EXIT_FAILURE);
     }
-
+    
     printf("The File name is = \"%s\" \n\n", argv[1]);
-
+    
     while((d = fgetc(fp)) != EOF){
         //buff[n] = d;
         n++;
         // printf("%c", d);
     }
-
+    
     rewind(fp);
-
+    
     char *buff = (char*) malloc(n*sizeof(char));
-    char *nestObj;
-
+    
+    
     int i=0;
-
+    
     while((d = fgetc(fp)) != EOF){
         buff[i] = d;
         i++;
     }
-
-    Parser(0,n-1,buff);
+    
+    Parser(n,start , buff);
     return 0;
 }
 
-void Parser(int start, int end, char *buff)
+void Parser(int size, int startp, char *buff)
 {
-    int i = start;
+    int i = 0;
     int j = 0;
     int objNested = 0;
     int objSize = 0;
-
-//    while(i != size){
-//        //buff[n] = d;
-//        // n++;
-//
-//        printf("buff[%d] = %c\n", i,buff[i]);
-//        i++;
-//
-//    }
-//
-   // i=0;
+    
+    //    while(i != size){
+    //        //buff[n] = d;
+    //        // n++;
     //
-
-    if(buff[0] != '{'){
+    //        printf("buff[%d] = %c\n", i,buff[i]);
+    //        i++;
+    //
+    //    }
+    //
+    i=startp;
+    //
+    
+    if(buff[i] != '{'){
         printf("in if \n");
         return;
     }
     i++;
-
+    
     //  printf("%d", size);
-
-    while(i < end+1){
+    
+    while(i < size){
         // printf("while\n");
-
+        
         switch ( buff[i] ) {
             case '"':
                 // printf("1");
@@ -130,27 +132,26 @@ void Parser(int start, int end, char *buff)
                 // printf("3");
                 token.end = i;
                 token.size = token.end - token.start+1;
-
-
+                
+                
                 for(int a = token.start; a<token.end; a++)
                 {
                     printf("%c",buff[a]);
-
+                    
                 }
                 printf(" : ");
-                printf("%d\n",token.type);
-               // printf("token size = %d\n",token.size);
-                printf("start = %d, end =%d \n", token.start, token.end);
+                printf(" (range : %d ~%d , type : %d) \n", token.start, token.end, token.type);
                 break;
-
+                
             case '{':
-
-
-
+                
+                
+                
                 token.start = i ;
                 token.type = OBJECT;
                 j = i;
                 while(buff[j] != '}'  ){
+                    
                     if(buff[j] == '{') objNested++;
                     if(buff[j] == '}') objNested--;
                     if(objNested == 0) break;
@@ -159,23 +160,35 @@ void Parser(int start, int end, char *buff)
                 token.end = j;
                 objSize = (j - i);
                 char *nestObj = (char*) malloc(objSize*sizeof(char));
-
-                for(int s = 0 ; s < objSize; s++){
-                    nestObj[s] = buff[i++];
-
-                }
-
-                Parser(i, i+objSize-1, nestObj);
                 
-
+                for(int s = token.start ; s <= token.end; s++){
+                    
+                    nestObj[s] = buff[i++];
+                    
+                    
+                }
+                
+                for(int a = token.start; a<=token.end; a++)
+                {
+                    printf("%c",buff[a]);
+                    
+                }
+                
+                printf(" (range : %d ~%d , type : %d) \n", token.start, token.end, token.type);
+                
+                
+                
+                Parser(token.start + objSize, token.start, nestObj);
+                
+                
                 break;
-
+                
             default: break;
-
-
+                
+                
         }
         i++;
     }
-
-
+    
+    
 }
